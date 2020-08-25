d16527f9e93fe4a048add7f73ef35f46fe4ea582
kylelawson <kyle.lawson.2016@uni.strath.ac.uk>
2020-08-25 16:12:50 +0100

    + Updated the eboxes collection to have a data field in each ebox entry. This will hold the data for voltage/power etc. It is an array of JSONs which have an entry for each type of data and the timestamp for this packet.

--- common/config/env.config.js
+++ common/config/env.config.js
@@ -2,7 +2,7 @@ module.exports = {
     "port": 3600,
     "appEndpoint": "http://localhost:3600",
     "apiEndpoint": "http://localhost:3600",
-    "jwt_secret": "myS33!!creeeT",
+    "jwt_secret": "94C^&OguPM$Utd^o",
     "jwt_expiration_in_seconds": 36000,
     "environment": "dev",
     "permissionLevels": {
